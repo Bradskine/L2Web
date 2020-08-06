a9b212560d81dfc4c160c95fc287539e8c85a154
Bradskine <51344446+Bradskine@users.noreply.github.com>
2020-08-06 21:08:19 +1200

    Add files via upload
    almost finished website

--- code.js
+++ code.js
@@ -42,20 +42,33 @@ var options = ['music', 'art', 'Board Games', 'sports', 'meet survivers']
 
 var imgs = ['images/music.jpg', 'images/art.jpg', 'images/boardGame.jpg', 'images/sports.jpg', 'images/meeting.jpg']
 
-
-
+//arrays with diffirent images with subtitles mathcing
 
 
 function things() {
   
-   var rand = Math.floor(Math.random() * options.length);
+   var rand = Math.floor(Math.random() * options.length); //gets a random number based on lenth of arrays
 
 
     document.getElementById('text').innerHTML = options[rand];
     document.getElementById('thingsImg').src = imgs[rand];
     document.getElementById('thingsImg').style.border = '0.2vw solid white';
-    
+    //changes html and css based of what the randomley selected number from array is made
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
